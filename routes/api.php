a623ca55d491d2137f342bf55e6682ffed42692d
song-ouyang <1723767677@qq.com>
2021-08-25 11:19:24 +0800

    oys第五次

--- routes/api.php
+++ routes/api.php
@@ -20,41 +20,25 @@ Route::prefix('sds')->namespace('Career')->group(function (){
    Route::post('remove','SdsController@sdsRemove');//点击重做将分数归零
 });//zcr
 
-<<<<<<< HEAD:laravel-master/routes/api.php
 
 
 
 Route::prefix('pdp')->namespace('Career')->group(function () {
-=======
-Route::prefix('test/pdp')->namespace('Career')->group(function () {
->>>>>>> f989bf86f5a705c01d9a842f59edb722fea3a85d:routes/api.php
     Route::get('num', 'PdpController@pdpNum');//查看完成pdp的人数
     Route::post('score', 'PdpController@pdpScore');//录入分数 把分数录入到数据库
     Route::post('result', 'PdpController@pdpResult');//查看分数并返回结果
     Route::post('remove', 'PdpController@pdpRemove');//点击重做将分数归零
-<<<<<<< HEAD:laravel-master/routes/api.php
 }); //wzh
 
 //气质
 Route::prefix('Tem')->namespace('Career')->group(function (){
-=======
-});
-//wzh
-Route::prefix('test/Tem')->namespace('Career')->group(function (){
->>>>>>> f989bf86f5a705c01d9a842f59edb722fea3a85d:routes/api.php
     Route::get('num','TemperamentController@Temnum');//查看气质测试的完成人数
     Route::post('score','TemperamentController@Temscore');//写入分数
     Route::post('result','TemperamentController@Temresult');//查看结果并返回
     Route::post('remove','TemperamentController@Temremove');//点击重做将分数归零
-<<<<<<< HEAD:laravel-master/routes/api.php
 }); //gjy
 
 
-=======
-});
-//gjy
-     Route::view('addd', 'add');
->>>>>>> f989bf86f5a705c01d9a842f59edb722fea3a85d:routes/api.php
 //后台
 Route::prefix('home')->namespace('Admin')->group(function ()
 {
