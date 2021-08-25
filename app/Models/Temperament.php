d89911a160053576ef9ba706f77f8a69acac2bab
song-ouyang <1723767677@qq.com>
2021-08-25 16:09:44 +0800

    gjy第三次

--- app/Models/Temperament.php
+++ app/Models/Temperament.php
@@ -26,8 +26,8 @@ class Temperament extends Model
     */
     /***返回Temperament总结果
      *某一项气质大于20，则为典型气质；气质在10-20则为一般气质；气质低于10分则为气质表达不明显；两种分差不大于四分则是混合性气质；如果某一项低于0则是表明排斥性
-     * @param Request $request
-     * @return $date1
+     * @param bile_score,bloody_score,mucus_score,depression_score
+     * @return $res
      * @author gujiayu
      */
     public static function gjy_result($result)
@@ -37,30 +37,39 @@ class Temperament extends Model
             $bloody_score = $result->get('bloody_score');
             $mucus_score = $result->get('mucus_score');
             $depression_score = $result->get('depression_score');
-            $sum=array($bile_score,$bloody_score,$mucus_score,$depression_score);
+            $sum=array("bile_score"=>$bile_score,"bloody_score"=>$bloody_score,"mucus_score"=>$mucus_score,"depression_score"=>$depression_score);
+            $sum1=array($bile_score,$bloody_score,$mucus_score,$depression_score);
             $date= array();
-            for($i=0;$i<4;$i++){
-
-                if($sum[$i]>20){
-                    array_push($date,"你的典型气质是".$sum[$i]);
-                }
-                if($sum[$i]>=10&&$sum[$i]<=20){
-                    array_push($date,"你的一般型气质是".$sum[$i]);
-                }
-                if($sum[$i]<10&&$sum[$i]>=0){
-                    array_push($date,"你的一般型气质是".$sum[$i]);
-                }
-                if($sum[$i]<0){
-                    array_push($date,"你的".$sum[$i]."表现排斥性");
-                }
-                for($j=$i;$j<=3;$j++){
-                    if($sum[$i]-$sum[$j+1]==4||$sum[$i]-$sum[$j+1]==-4){
-                        array_push($date,"你的混合型气质有".$sum[$i]."和".$sum[$j+1]);
-                    }
+            for($j=0;$j<4;$j++){
+                for($i=$j+1;$i<4;$i++){
+                    if($sum1[$j]-$sum1[$i] >=-4 && $sum1[$j] - $sum1[$j+1] <=4){
+                        $date1 = $sum1[$j];
+                        $date2 = $sum1[$i];
+                    }}
+            }
+            foreach($sum as $name=>$value){
+                if($value == $date1||$value == $date2){
+                    array_push($date,"你的混合性气质是".$name);
                 }
             }
-            $date1 = implode(" ",$date);
-            return $date1;
+            foreach ($sum as $name=>$value){
+                if($value > 20) {
+                    array_push($date, "你的典型气质是" . $name);
+                }
+                if($value >=10 &&$value<=20)
+                {
+                    array_push($date,"你的一般型气质是".$name);
+                }
+                if($value >=0&& $value<10){
+                    array_push($date,"你的气质表现不明显的是".$name);
+                }
+                if($value<0){
+                    array_push($date,"你的".$name."表现排斥性");
+                }
+            }
+
+            $res = implode(" ",$date);
+            return $res;
         } catch (\Exception $e) {
             logError('获取用户信息错误', [$e->getMessage()]);
             return null;
